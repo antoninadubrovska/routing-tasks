41d32ba68c1f66c2b38c6e98d1b4e93bbbfb1bac
Antonina Dubrovska <antonina.dubrovska@gmail.com>
2026-03-26 21:37:45 +0100

    Updated styles for products view

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,8 +1,17 @@
+import './start.css'
+
 const Start = () => {
 	return (
 		<div>
 			<h2> Välkommen till oss! </h2>
-			<p> start-komponenten </p>
+			<p>
+				{" "}
+				Hos oss hittar du tandborstar för alla leenden – stora som små!
+				Vi brinner för friska tänder och gör det enkelt (och lite
+				roligare 😉) att ta hand om din munhälsa. Oavsett om du föredrar
+				klassiska tandborstar eller smarta eltandborstar har vi något
+				för dig. Borsta bättre, le mer! 😁{" "}
+			</p>
 		</div>
 	);
 };
